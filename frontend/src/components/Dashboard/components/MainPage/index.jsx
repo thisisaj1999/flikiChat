d8b2129599591a86c2d6b228fc9a7439c6b66fb2
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-21 22:03:35 +0400

    [Update]: Send message field

--- frontend/src/components/Dashboard/components/MainPage/index.jsx
+++ frontend/src/components/Dashboard/components/MainPage/index.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MainPage.module.scss";
 
-import { Divider, Input, Button, Avatar} from 'antd';
+import { Divider, Input, Button, Avatar, Form } from "antd";
 const { TextArea } = Input;
 
-import Send from '../../../../assets/send.svg'
+import Send from "../../../../assets/send.svg";
 
 import { useGlobalStore } from "../../../../utils/store";
 
 const index = () => {
+	const [form] = Form.useForm();
+	const [messages, setMessages] = useState([]);
+
+
 
 	const Update = {
 		GlobalStore: {
-			isGroupInfoOpen: useGlobalStore((State) => State.setIsGroupInfoOpen),
+			isGroupInfoOpen: useGlobalStore(
+				(State) => State.setIsGroupInfoOpen
+			),
 		},
 	};
 
@@ -22,7 +28,7 @@ const index = () => {
 		},
 	};
 
-	const handleOpenGroupInfo = () => Update.GlobalStore.isGroupInfoOpen(true)
+	const handleOpenGroupInfo = () => Update.GlobalStore.isGroupInfoOpen(true);
 
 	const GroupOpenInfoWidth = {
 		width: "calc(100% - 30rem)",
@@ -33,10 +39,25 @@ const index = () => {
 		width: "calc(100% - 15rem)",
 		transition: "width 0.3s ease-out",
 	};
-	
+
+	const onFinish = (values) => {
+		console.log(values);
+		form.resetFields();
+	};
+
 	return (
-		<div className={styles.DashboardMainPage} style={State.GlobalStore.isGroupInfoOpen ? GroupOpenInfoWidth : GroupCloseInfoWidth}>
-			<div className={styles.MainPageHeading} onClick={handleOpenGroupInfo}>
+		<div
+			className={styles.DashboardMainPage}
+			style={
+				State.GlobalStore.isGroupInfoOpen
+					? GroupOpenInfoWidth
+					: GroupCloseInfoWidth
+			}
+		>
+			<div
+				className={styles.MainPageHeading}
+				onClick={handleOpenGroupInfo}
+			>
 				<Avatar
 					style={{
 						backgroundColor: "dodgerblue",
@@ -49,103 +70,72 @@ const index = () => {
 				</Avatar>
 				<div className={styles.GroupDetails}>
 					<p className={styles.GroupName}>Test Group</p>
-					<p className={styles.GroupParticipantName}>Test, Test, Test</p>
+					<p className={styles.GroupParticipantName}>
+						Test, Test, Test
+					</p>
 				</div>
 			</div>
 
 			<Divider style={{ width: "100%", margin: "0px" }} />
 
 			<div className={styles.MainPageContent}>
-			{/* Dummy Chats */}
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleLeft}`}>
+				{/* Dummy Chats */}
+				<div
+					className={`${styles.ChatBubble} ${styles.ChatBubbleLeft}`}
+				>
 					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
+						<p>
+							This one adds a right triangle on the left, flush at
+							the top by using .tri-right and .left-top to specify
+							the location.
+						</p>
 					</div>
 					<p className={styles.TimeStamp}>5:34 pm</p>
 				</div>
 
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleRight}`}>
+				<div
+					className={`${styles.ChatBubble} ${styles.ChatBubbleRight}`}
+				>
 					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
+						<p>
+							This one adds a right triangle on the left, flush at
+							the top by using .tri-right and .left-top to specify
+							the location.
+						</p>
 					</div>
 					<p className={styles.TimeStamp}>5:36 pm</p>
 				</div>
-				
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleLeft}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>5:36 pm</p>
-				</div>
-
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleRight}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>5:40 pm</p>
-				</div>
-				
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleLeft}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>6:04 pm</p>
-				</div>
-
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleRight}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>6:10 pm</p>
-				</div>
-				
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleLeft}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>6:04 pm</p>
-				</div>
-
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleRight}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>6:10 pm</p>
-				</div>
-				
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleLeft}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>6:04 pm</p>
-				</div>
-
-				<div className={`${styles.ChatBubble} ${styles.ChatBubbleRight}`}>
-					<div className={styles.ChatText}>
-						<p>This one adds a right triangle on the left, flush at the top by using .tri-right and .left-top to specify the location.</p>
-					</div>
-					<p className={styles.TimeStamp}>6:10 pm</p>
-				</div>
-
 			</div>
 
 			<Divider style={{ width: "100%", margin: "0px" }} />
 
-			<div className={styles.MainPageFooter}>
-				<div className={styles.TextHolder}>
-					<TextArea
-						className={styles.TextAreaInput}
-						placeholder="Type here so engage in the chat"
-						autoSize
-					/>
-				</div>
-				<div className={styles.SendChatBtn}>
-					<Button type="primary" shape="circle">
-						<img src={Send} alt="" width={20} />
-					</Button>
-				</div>
+			{/* <div > */}
+				<Form
+					layout="vertical"
+					form={form}
+					onFinish={onFinish}
+					className={styles.MainPageFooter}
+				>
+					{/* <div className={styles.TextHolder}> */}
+						<Form.Item
+							name="message"
+							className={styles.TextHolder}
+						>
+							<TextArea
+								className={styles.TextAreaInput}
+								placeholder="Type here so engage in the chat"
+								autoSize
+							/>
+						</Form.Item>
+					{/* </div> */}
+					<div className={styles.SendChatBtn}>
+						<Button type="primary" shape="circle" htmlType="submit">
+							<img src={Send} alt="" width={20} />
+						</Button>
+					</div>
+				</Form>
 			</div>
-		</div>
+		// </div>
 	);
 };
 
