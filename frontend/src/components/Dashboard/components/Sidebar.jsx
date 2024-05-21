2b534901171cbff7289a10beeb4758fdeeae557a
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-21 13:26:40 +0400

    [Add]: UI Changes to Sidebar

--- frontend/src/components/Dashboard/components/Sidebar.jsx
+++ frontend/src/components/Dashboard/components/Sidebar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "../Dashboard.module.scss";
 
-import { Button, Avatar } from 'antd';
+import { Button, Avatar, Dropdown, Divider } from 'antd';
 
 import Menu from '../../../assets/menu.svg'
+import Logout from '../../../assets/logout.svg'
 
 const Sidebar = () => {
+
+	const MenuItems = [
+		{
+			label: <>Sign Out</>,
+			key: '0',
+			icon: <img src={Logout} alt="Sign Out" width={20}/>
+		},
+	];
+
 	return (
 		<div className={styles.DashboardSidebar}>
 			<div className={styles.SidebarGroups}>
@@ -21,10 +31,20 @@ const Sidebar = () => {
 						Y
 					</Avatar>
 
-					<Button type="text" shape="circle">
-						<img src={Menu} alt="" width={20} />
-					</Button>
+					<Dropdown
+						menu={{
+							items: MenuItems,
+						}}
+						placement="bottomRight"
+						trigger={['click']}
+					>
+						<Button type="text" shape="circle">
+							<img src={Menu} alt="" width={20} />
+						</Button>
+					</Dropdown>
 				</div>
+
+				<p className={styles.GroupsHeading}>Groups</p>
 
 				<div className={styles.ListGroupMain}>
 					<div className={styles.ListGroup}>
@@ -44,7 +64,7 @@ const Sidebar = () => {
 						</div>
 					</div>
 
-					{/* <Divider style={{width: '100%', margin: '0px'}}/> */}
+					<Divider style={{width: '100%', margin: '0px'}}/>
 
 					<div className={styles.ListGroup}>
 						<Avatar
@@ -62,6 +82,10 @@ const Sidebar = () => {
 							<p>Test Group1</p>
 						</div>
 					</div>
+
+					<Divider style={{width: '100%', margin: '0px'}}/>
+
+					
 				</div>
 			</div>
 
