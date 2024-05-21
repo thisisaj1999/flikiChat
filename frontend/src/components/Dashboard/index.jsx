f7ba087c82b837710b1728c4b8261b42511d721a
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-21 10:07:41 +0400

    [Add]: Chat Header

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import styles from "./Dashboard.module.scss";
 
+import { Divider } from 'antd';
+
+
 const index = () => {
 	return (
 		<div className={styles.DashboardBgMain}>
-			<div className={styles.DashboardChatMain}>test</div>
+			<div className={styles.DashboardChatMain}>
+				<div className={styles.DashboardSidebar}>Sidebar</div>
+				<Divider type="vertical" style={{height: '100%', margin: '0px'}}/>
+				<div className={styles.DashboardMainPage}>
+					<div className={styles.MainPageHeading}>
+						<p className={styles.GroupName}>Test Group</p>
+						<p className={styles.GroupParticipantName}>Test, Test, Test</p>
+					</div>
+					<Divider style={{width: '100%', margin: '0px'}}/>
+					<div className={styles.MainPageContent}>
+						chats
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
