f53c10e5bfea7fdc8e092677e099f7f967a47325
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-23 09:42:45 +0400

    [Update]: registerUser data on registration

--- backend/controllers/userController/registerUser.js
+++ backend/controllers/userController/registerUser.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcryptjs');
 const registerUser = async (req, res) => {
 	const userData = req.body;
 
-	if (Object.keys(userData).length === 0) {
+	if (!userData || Object.keys(userData).length === 0) {
 		console.log(`🔴  registerUser : User's data is required for create`);
 		res.json({
 			status: 404,
@@ -17,16 +17,18 @@ const registerUser = async (req, res) => {
 		return;
 	}
 
-	const name = userData?.name;
-	const email = userData?.email;
-	const password = await bcrypt.hash(userData?.password, 15);
-	const joinned_group_ids = userData?.joinned_group_ids;
+	const { name, email, password, joinned_group_ids } = userData;
 
-	const insert_users_table = fs
-		.readFileSync(
-			path.join(__dirname, "../../sql/insert/insert_users_table.sql")
-		)
-		.toString();
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+
+	const insert_users_table = fs.readFileSync(
+		path.join(__dirname, "../../sql/insert/insert_users_table.sql")
+	).toString();
+
+	const user_login_data = fs.readFileSync(
+		path.join(__dirname, "../../sql/get/user_login_data.sql")
+	).toString();
 
 	try {
 		const isUserExists = await db.query(
@@ -36,34 +38,37 @@ const registerUser = async (req, res) => {
 
 		if (isUserExists?.rowCount === 0) {
 
-			const userValues = [name, email, password];
+			const userValues = [name, email, hashedPassword];
 
-			const create_newUser = await db.query(
-				insert_users_table,
-				userValues
-			);
+			const create_newUser = await db.query(insert_users_table, userValues);
 
 			if (create_newUser.rows.length > 0) {
 				const userId = create_newUser.rows[0].id;
 
 				// Insert joined group IDs into the user_groups table
 				for (const groupId of joinned_group_ids) {
-					await db.query(`INSERT INTO group_memberships (user_id, group_id) VALUES ($1, $2)`, [userId, groupId]);
+					await db.query(`INSERT INTO group_memberships (user_id, group_id, is_admin) VALUES ($1, $2, $3)`, [userId, groupId, false]);
 				}
 
 				// Authenticate user with jwt
-        const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+				const getGroupsAndMessages = await db.query(user_login_data, [userId]);
+
+				const token = jwt.sign({
+					id: userId,
+					name: name,
+					email: email,
+					groups: getGroupsAndMessages.rows
+				}, 
+				process.env.JWT_SECRET, 
+				{
 					expiresIn: process.env.JWT_REFRESH_EXPIRATION
-				});
+				})
 	
 				console.log(`🟢  registerUser : Data inserted to users table`);
 				res.json({
 					status: 200,
 					message: `Data inserted to users table`,
 					data: {
-						id: userId,
-						name: name,
-						email: email,
 						accessToken: token,
 					}
 				});
