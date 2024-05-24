e971e72a2219413c8ee43b325aa3c2cccc353d72
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-24 17:03:48 +0400

    [Temp]: production true

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const isProduction = false;
+const isProduction = true;
 const URL = isProduction ? `https://flikichat.onrender.com` : `http://localhost:8080`;
 
 const socket = io(URL, { autoConnect: false });
